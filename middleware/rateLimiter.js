687968c9cb28315347ffce08aa7f9256a832c427
Eric <ericlg.link@gmail.com>
2022-12-01 14:03:44 -0800

    Fix the syntax for the limiter error message

--- middleware/rateLimiter.js
+++ middleware/rateLimiter.js
@@ -3,7 +3,7 @@ const rateLimiter = require("express-rate-limit");
 const limiter = rateLimiter({
 max: 3,
 windowMS: 86000000, //24 Hours
-message: "You're out of pokeballs, come back tomorrow",
+message: { error: "You're out of pokeballs, come back tomorrow" },
 });
 
 module.exports = limiter
